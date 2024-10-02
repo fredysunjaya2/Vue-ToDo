3b2e39ce91744c675115160777d8c4d97d9ecba7
lofigirl <fredysunjaya0@gmail.com>
2024-10-02 16:17:48 +0700

    delete task,change status task

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, reactive, onBeforeMount, onMounted } from 'vue';
 
 const list = reactive({
   tasks: [
@@ -29,8 +29,8 @@ const list = reactive({
 const ongoingColor = ref("red-lighten-4");
 const doneColor = ref("green-darken-2");
 
-const ongoingTask = [];
-const doneTask = [];
+const ongoingTask = reactive([]);
+const doneTask = reactive([]);
 
 list.tasks.forEach((task) => {
   if (task.status === 'ongoing') {
@@ -40,33 +40,76 @@ list.tasks.forEach((task) => {
   }
 })
 
-const updateTask = (id) => {
-  list.tasks.sc
+const updateTask = (item, index) => {
+  if (item.status === 'ongoing') {
+    item.status = 'done';
+    const task = ongoingTask.splice(index, 1);
+    doneTask.push(task[0]);
+  } else if (item.status === 'done') {
+    item.status = 'ongoing';
+    const task = doneTask.splice(index, 1);
+    ongoingTask.push(task[0]);
+  }
 }
+
+const deleteTask = (item, index) => {
+    if (item.status === 'ongoing') {
+      ongoingTask.splice(index, 1);
+    } else if (item.status === 'done') {
+      doneTask.splice(index, 1);
+
+    }
+  }
+
 </script>
 
 <template>
-  <v-card class="mx-auto mt-16" max-width="1000" :color="ongoingColor">
-    <v-card-title>
-      
-    </v-card-title>
-    <v-card-item>
-      <v-list :bg-color="ongoingColor" class="border-t-md">
-        <v-list-item v-for="item in ongoingTask" :key="item.id" :value="item.id" class="border-b-md">
-          <p v-if="item.status === 'ongoing'">{{ item.name }}</p>
-        </v-list-item>
-      </v-list>
-    </v-card-item>
-  </v-card>
-  <v-card title="Done" class="mx-auto mt-16" max-width="1000" :color="doneColor">
-    <v-card-item>
-      <v-list :bg-color="doneColor">
-        <v-list-item v-for="item in doneTask" :key="item.id" :value="item.id">
-          <p>{{ item.name }}</p>
-        </v-list-item>
-      </v-list>
-    </v-card-item>
-  </v-card>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1 class="text-h3 text-center">My ToDo List</h1>
+      </v-col>
+    </v-row>
+    <v-row >
+      <v-col cols="9" align="end">
+        <v-btn prepend-icon="$mdiPlus" variant="outlined" class="mx-auto">
+          Add Task
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-card class="mx-auto mt-16" width="1000" :color="ongoingColor">
+        <v-card-title class="py-0 pt-3">
+          <p>Ongoing</p>
+        </v-card-title>
+        <v-card-item class="pt-3 pb-6">
+          <v-list :bg-color="ongoingColor" class="border-t-lg">
+            <v-list-item v-for="(item, index) in ongoingTask" :key="item.id" :value="item.id" class="border-b-md"
+              @click="updateTask(item, index)">
+              <div class="d-flex justify-space-between">
+                <p v-if="item.status === 'ongoing'">{{ item.name }}</p>
+                <v-btn variant="tonal" icon="$mdiClose" @click="deleteTask(item, index)">
+                </v-btn>
+              </div>
+            </v-list-item>
+          </v-list>
+        </v-card-item>
+      </v-card>
+      <v-card class="mx-auto mt-16" width="1000" :color="doneColor">
+        <v-card-title class="" py-0 pt-3>
+          <p>Done</p>
+        </v-card-title>
+        <v-card-item class="pt-3 pb-6">
+          <v-list :bg-color="doneColor" class="border-t-lg">
+            <v-list-item v-for="(item, index) in doneTask" :key="item.id" :value="item.id" class="border-b-md"
+              @click="updateTask(item, index)">
+              <p>{{ item.name }}</p>
+            </v-list-item>
+          </v-list>
+        </v-card-item>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <style></style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,9 +1,13 @@
 import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import { createApp } from 'vue'
 
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import { mdiClose, mdiPlus } from '@mdi/js'
+
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
@@ -11,6 +15,17 @@ import * as directives from 'vuetify/directives'
 import App from './App.vue'
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases: {
+      ...aliases,
+      mdiClose: mdiClose,
+      mdiPlus: mdiPlus,
+    },
+    sets: {
+      mdi,
+    },
+  },
   components,
   directives,
 })
