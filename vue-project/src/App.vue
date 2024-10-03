d3ec4a585f8a7dba9cedcb99d6128e046b7cb1c7
FREDY SUNJAYA <U554402@bca.co.id>
2024-10-03 17:35:25 +0700

    add task, form vuetify, reading json

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,36 +1,18 @@
 <script setup>
-import { ref, reactive, onBeforeMount, onMounted } from 'vue';
+import { ref, reactive, onBeforeUnmount, onMounted } from 'vue';
+import tasks from '@/tasks.json'
 
-const list = reactive({
-  tasks: [
-    {
-      id: "e0303f61ef3f9",
-      name: "Testing",
-      status: "done"
-    },
-    {
-      id: "d80c1ffbe9a2f",
-      name: "item 2",
-      status: "ongoing"
-    },
-    {
-      id: "af966bae04967",
-      name: "item 3",
-      status: "ongoing"
-    },
-    {
-      id: "47ae9eeece4c4",
-      name: "item 4",
-      status: "ongoing"
-    },
-  ]
-});
+const list = reactive(tasks);
 
 const ongoingColor = ref("red-lighten-4");
 const doneColor = ref("green-darken-2");
 
 const ongoingTask = reactive([]);
 const doneTask = reactive([]);
+
+let taskTitle = ref("");
+const formValid = ref(false);
+const isDialogShown = ref(false);
 
 list.tasks.forEach((task) => {
   if (task.status === 'ongoing') {
@@ -53,22 +35,47 @@ const updateTask = (item, index) => {
 }
 
 const deleteTask = (item, index) => {
-    if (item.status === 'ongoing') {
-      ongoingTask.splice(index, 1);
-    } else if (item.status === 'done') {
-      doneTask.splice(index, 1);
-    }
+  if (item.status === 'ongoing') {
+    ongoingTask.splice(index, 1);
+  } else if (item.status === 'done') {
+    doneTask.splice(index, 1);
   }
+}
 
 const addTask = () => {
-  const task = {
-    id: Math.random().toString(16).slice(2),
-    name: 'asd',
-    status: 'ongoing'
-  }
+  if (formValid.value) {
 
-  ongoingTask.push(task);
+    const task = {
+      id: Math.random().toString(16).slice(2),
+      name: taskTitle.value,
+      status: 'ongoing'
+    }
+
+    ongoingTask.push(task);
+
+    isDialogShown.value = false;
+    taskTitle.value = "";
+  }
 }
+
+const rules = {
+  taskRules: [
+    (value) => {
+      if (value) return true;
+
+      return "Task can't be empty";
+    },
+    (value) => {
+      if (value.length < 20) return true;
+
+      return "Task Name can't be more than 20 characters"
+    }
+  ]
+}
+
+onBeforeUnmount(() => {
+  alert('here');
+});
 
 </script>
 
@@ -81,20 +88,50 @@ const addTask = () => {
     </v-row>
     <v-row justify="center">
       <v-col align="end" class="" cols="8">
-        <v-btn prepend-icon="$mdiPlus" variant="outlined" class="mx-auto" @click="addTask">
-          Add Task
-        </v-btn>
+        <v-dialog persistent width="50%" v-model="isDialogShown">
+          <template v-slot:activator="{ props: activatorDialog }">
+            <v-btn v-bind="activatorDialog" prepend-icon="$mdiPlus" variant="outlined" class="mx-auto">
+              Add Task
+            </v-btn>
+          </template>
+          <template v-slot:default="{ isActive }">
+            <v-card>
+              <v-card-item>
+                <v-card-title>
+                  Add New Task
+                </v-card-title>
+              </v-card-item>
+              <v-card-text>
+                <v-container max-width="100%">
+                  <v-row>
+                    <v-col>
+                      <v-form validate-on="input lazy" @submit.prevent="addTask();" v-model="formValid">
+                        <v-text-field clearable placeholder="Type Task" type="text" v-model="taskTitle"
+                          :rules="rules.taskRules">
+
+                        </v-text-field>
+                        <v-btn type="submit">
+                          Add
+                        </v-btn>
+                      </v-form>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+            </v-card>
+          </template>
+        </v-dialog>
       </v-col>
     </v-row>
     <v-row justify="center">
       <v-col cols="8">
         <v-card class="mx-auto mt-8" :color="ongoingColor">
-          <v-card-title class="py-0 pt-3">
-            <p>Ongoing</p>
-          </v-card-title>
           <v-card-item class="pt-3 pb-6">
+            <v-card-title class="py-0 pt-3">
+              <p>Ongoing</p>
+            </v-card-title>
             <v-list :bg-color="ongoingColor" class="border-t-lg">
-              <v-list-item v-for="(item, index) in ongoingTask" :key="item.id" :value="item.id" class="border-b-md"
+              <v-list-item v-for="( item, index ) in  ongoingTask " :key="item.id" :value="item.id" class="border-b-md"
                 @click="updateTask(item, index)">
                 <div class="d-flex justify-space-between align-center">
                   <p>{{ item.name }}</p>
@@ -106,12 +143,12 @@ const addTask = () => {
           </v-card-item>
         </v-card>
         <v-card class="mx-auto mt-8" :color="doneColor">
-          <v-card-title class="" py-0 pt-3>
-            <p>Done</p>
-          </v-card-title>
           <v-card-item class="pt-3 pb-6">
+            <v-card-title class="" py-0 pt-3>
+              <p>Done</p>
+            </v-card-title>
             <v-list :bg-color="doneColor" class="border-t-lg">
-              <v-list-item v-for="(item, index) in doneTask" :key="item.id" :value="item.id" class="border-b-md"
+              <v-list-item v-for="( item, index ) in  doneTask " :key="item.id" :value="item.id" class="border-b-md"
                 @click="updateTask(item, index)">
                 <div class="d-flex justify-space-between align-center">
                   <p>{{ item.name }}</p>
